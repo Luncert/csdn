7067624c83030b6dfb079981a2f48daa801cdf3c
Luncert <2725115515@qq.com>
2019-02-24 23:58:57 +0800

    .

--- csdn2/src/main/java/org/luncert/csdn2/App.java
+++ csdn2/src/main/java/org/luncert/csdn2/App.java
@@ -5,6 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * TODO: shutdown with shutcut
+ */
 @SpringBootApplication
 public class App implements CommandLineRunner {
 
